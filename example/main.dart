0d6dca8258801eb3114a4b1043f84c5c452ce606
Michael Lazebny <michael@lazebny.io>
2024-02-04 09:39:58 +0100

    Added some points to consider

---
+++ example/main.dart
@@ -0,0 +1,36 @@
+import 'package:http/http.dart';
+import 'package:intercepted_client/intercepted_client.dart';
+
+class AuthInterceptor extends SequentialHttpInterceptor {
+  final String token;
+
+  AuthInterceptor(this.token);
+
+  Future<String> refreshToken() async {
+    // Refresh token
+    return 'newToken';
+  }
+
+  @override
+  void interceptRequest(BaseRequest request, RequestHandler handler) async {
+    var t = token;
+
+    // check if token is expired, if expired refresh token
+    if (token == 'expiredToken') {
+      t = await refreshToken();
+    }
+
+    request.headers['Authorization'] = 'Bearer $t';
+
+    handler.next(request);
+  }
+
+  @override
+  void interceptResponse(Response response, ResponseHandler handler) {
+    if (response.statusCode == 401) {
+      // Refresh token and retry request
+    }
+
+    handler.next(response);
+  }
+}
